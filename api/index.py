f96f339c468c2bc908cb6f1b25e52cbfc073bf04
notfenixio <103071021+NotFenixio@users.noreply.github.com>
2024-01-20 20:08:31 +0100

    auth

---
+++ api/index.py
@@ -0,0 +1,43 @@
+from base64 import b64encode
+from os import getenv
+from flask import Flask, redirect, request, session, render_template
+from requests import get
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+app.secret_key = getenv("secretkey")
+
+def base64(string):
+    return b64encode(string.encode("utf-8")).decode()
+
+@app.get("/")
+def home():
+    return render_template("index.html")
+
+@app.get("/auth")
+def auth():
+    if "username" not in session:
+        return redirect(f"https://auth.itinerary.eu.org/auth/?redirect={ base64('https://scratchauthdemo.vercel.app/authenticate') }&name=NotFenixio%27s%20ScratchAuth%20Example")
+    else:
+        return render_template("auth.html", username=session["username"])
+
+@app.get("/authenticate")
+def authenticate():
+    code = request.args.get("privateCode")
+    
+    if code == None:
+        return "Bad Request", 400
+
+    response = get(f"https://auth.itinerary.eu.org/api/auth/verifyToken?privateCode={privateCode}").json()
+    if response["redirect"] == "https://scratchauthdemo.vercel.app/authenticate":
+        if response["valid"]:
+            session["username"] = response["username"]
+            return redirect("/auth")
+        else:
+            return f"Authentication failed!"
+    else:
+        return "Invalid Redirect", 400
+
+app.run(host='0.0.0.0', port=8080)
