28408cc7e5f980c273db47da9dfc3c6460e8a6fa
kRxZykRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-04-02 16:33:21 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = os.urandom(24)
+app.secret_key = urandom(24)
 
 """
 Thanks to Chiroyce (https://replit.com/@Chiroyce/auth) for part of the code! Truly the GOAT.
@@ -16,36 +16,37 @@ Thanks to Chiroyce (https://replit.com/@Chiroyce/auth) for part of the code! Tru
 def base64(string):
     return b64encode(string.encode("utf-8")).decode()
 
+def btoa(string):
+    return b64encode(string.encode()).decode()
+
 @app.get("/")
 def home():
     return render_template("index.html")
 
-def btoa(string):
-    return base64.b64encode(string.encode()).decode()
-
 @app.get("/auth")
 def auth():
     if "username" not in session:
-        return redirect(f"https://auth.itinerary.eu.org/auth/?redirect={ base64('https://scratch-auth-demo.vercel.app/authenticate') }&name=NotFenixio%27s%20ScratchAuth%20Example")
+        return redirect(f"https://auth.itinerary.eu.org/auth/?redirect={base64('https://scratch-auth-demo.vercel.app/authenticate')}&name=NotFenixio%27s%20ScratchAuth%20Example")
     else:
-        return render_template("auth.html", username=session["username"])
-        return redirect(f"https://scratch-coding-hut.github.io/account?username={btoa(session["username"])}")
+        return redirect(f"https://scratch-coding-hut.github.io/account?username={btoa(session['username'])}")
 
 @app.get("/authenticate")
 def authenticate():
     code = request.args.get("privateCode")
     
-    if code == None:
+    if code is None:
         return "Bad Request", 400
 
     response = get(f"https://auth.itinerary.eu.org/api/auth/verifyToken?privateCode={code}").json()
-    if response["redirect"] == "https://scratch-auth-demo.vercel.app/authenticate":
-        if response["valid"]:
+    
+    if response.get("redirect") == "https://scratch-auth-demo.vercel.app/authenticate":
+        if response.get("valid"):
             session["username"] = response["username"]
             return redirect("/auth")
         else:
-            return f"Authentication failed!"
+            return "Authentication failed!"
     else:
         return "Invalid Redirect", 400
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
