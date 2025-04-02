167f5088203cb09404991cdc5ba3ea0604901162
kRxZykRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-04-02 07:53:43 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,5 +1,5 @@
 from base64 import b64encode
-from os import getenv
+from os import getenv, urandom
 from flask import Flask, redirect, request, session, render_template
 from requests import get
 from dotenv import load_dotenv
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = getenv("secretkey")
+app.secret_key = os.urandom(24)
 
 """
 Thanks to Chiroyce (https://replit.com/@Chiroyce/auth) for part of the code! Truly the GOAT.
@@ -20,12 +20,16 @@ def base64(string):
 def home():
     return render_template("index.html")
 
+def btoa(string):
+    return base64.b64encode(string.encode()).decode()
+
 @app.get("/auth")
 def auth():
     if "username" not in session:
         return redirect(f"https://auth.itinerary.eu.org/auth/?redirect={ base64('https://scratch-auth-demo.vercel.app/authenticate') }&name=NotFenixio%27s%20ScratchAuth%20Example")
     else:
         return render_template("auth.html", username=session["username"])
+        return redirect(f"https://scratch-coding-hut.github.io/account?username={btoa(username=session["username"])}")
 
 @app.get("/authenticate")
 def authenticate():
